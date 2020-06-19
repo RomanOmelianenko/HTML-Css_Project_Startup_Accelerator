5083ab44f8ae3fd2094503f2573cb5f33f3d6b53
RomanOmelianenko <r_omelianenko@ukr.net>
2020-06-19 23:53:44 +0300

    responsive_html_css

---
+++ build/js/scripts.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var scrollTop = document.querySelector(".scrollTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTop.style.display = "block";
+  } else {
+    scrollTop.style.display = "none";
+  }
+}
+
+scrollTop.addEventListener('click', function (event) {
+  scrollToTop(1000);
+});
+
+function scrollToTop(duration) {
+  // cancel if already on top
+  if (document.scrollingElement.scrollTop === 0) return;
+  var totalScrollDistance = document.scrollingElement.scrollTop;
+  var scrollY = totalScrollDistance,
+      oldTimestamp = null;
+
+  function step(newTimestamp) {
+    if (oldTimestamp !== null) {
+      // if duration is 0 scrollY will be -Infinity
+      scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+      if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+      document.scrollingElement.scrollTop = scrollY;
+    }
+
+    oldTimestamp = newTimestamp;
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+}
